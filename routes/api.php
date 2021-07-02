e3584fa64126e2b7523bb65a05b0072f5365650b
tismenetski <tismenetski@gmail.com>
2021-07-02 10:53:36 +0300

    first commit

---
+++ routes/api.php
@@ -0,0 +1,28 @@
+<?php
+
+use App\Http\Controllers\Auth\ApiAuthController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::middleware('auth:api')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+Route::group(['middleware' => ['cors', 'json.response']], function () {
+    // ...
+
+    Route::post('/login', [ApiAuthController::class,'login'])->name('login.api');
+    Route::post('/register',[ApiAuthController::class,'register'])->name('register.api');
+    Route::post('/logout', [ApiAuthController::class,'logout'])->name('logout.api');
+});
